86f1d2bbb256a845991ab2c48df275d5845b19ce
nidhi <nidhitripathi30gkp@gmail.com>
2021-03-31 17:05:03 +0530

    second-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,23 @@ import { FormsModule }   from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {MatButtonModule} from '@angular/material/button';
 import { ContactUsComponent } from './module/contact-us/contact-us/contact-us.component';
+import { HomeComponent } from './module/home/home/home.component';
+import { PostDetailsComponent } from './module/post-details/post-details/post-details.component';
+import { BlogEntryComponent } from './module/blog-entry/blog-entry/blog-entry.component';
+import { AboutUsComponent } from './module/about-us/about-us/about-us.component';
+import {HeaderComponent} from './module/core/header/header.component';
+import {FooterComponent} from './module/core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactUsComponent,
-  
+    HomeComponent,
+    PostDetailsComponent,
+    BlogEntryComponent,
+    AboutUsComponent,
+    HeaderComponent,
+    FooterComponent
     
     
    
